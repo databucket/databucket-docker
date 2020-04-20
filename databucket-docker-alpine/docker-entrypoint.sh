1244bb99961b5797b08eaaa547faf7f9185f37ab
kslysz <kslysz@gmail.com>
2020-04-20 13:44:03 +0200

    exposed DB_PROPERTIES parameter

--- databucket-docker-alpine/docker-entrypoint.sh
+++ databucket-docker-alpine/docker-entrypoint.sh
@@ -1,6 +1,6 @@
 exec java \
     -Ddatabuket.title=${TITLE} \
-    -Dspring.datasource.url=jdbc:mysql://${DB_HOST}:${DB_PORT}/${DB_DATABASE} \
+    -Dspring.datasource.url=jdbc:mysql://${DB_HOST}:${DB_PORT}/${DB_DATABASE}${PROPERTIES} \
     -Dspring.datasource.username=${DB_USER} \
     -Dspring.datasource.password=${DB_PASSWORD} \
     -jar $1
