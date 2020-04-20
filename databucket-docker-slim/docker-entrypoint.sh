bcfcb308fbf74eff195ed76a0691bd4d0dc8b875
kslysz <kslysz@gmail.com>
2020-04-20 14:49:05 +0200

    added description fixed parameter name

--- databucket-docker-slim/docker-entrypoint.sh
+++ databucket-docker-slim/docker-entrypoint.sh
@@ -1,6 +1,6 @@
 exec java \
     -Ddatabuket.title=${TITLE} \
-    -Dspring.datasource.url=jdbc:mysql://${DB_HOST}:${DB_PORT}/${DB_DATABASE}${PROPERTIES} \
+    -Dspring.datasource.url=jdbc:mysql://${DB_HOST}:${DB_PORT}/${DB_DATABASE}${DB_PROPERTIES} \
     -Dspring.datasource.username=${DB_USER} \
     -Dspring.datasource.password=${DB_PASSWORD} \
     -jar $1
